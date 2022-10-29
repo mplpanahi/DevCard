b2be6c2aa9c17628e99a73d7d6b0fd42916cc2fc
mplpanahi <MPLPanahi@gmail.com>
2022-10-29 15:02:14 +0330

    Index & Contact Page Added

--- DevCard/Controllers/HomeController.cs
+++ DevCard/Controllers/HomeController.cs
@@ -6,19 +6,13 @@ namespace DevCard.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
