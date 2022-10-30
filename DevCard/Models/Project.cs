15ce8f00a89ad76b3b58810c448c10016c2afcd5
mplpanahi <MPLPanahi@gmail.com>
2022-10-30 05:50:40 +0330

    Project Section From Index Page Move to Partial View Project

---
+++ DevCard/Models/Project.cs
@@ -0,0 +1,22 @@
+﻿namespace DevCard.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+
+    }
+    //public class Article
+    //{
+    //    public int Id { get; set; }
+    //    public string Title { get; set; }
+    //    public string Description { get; set; }
+    //}
+    //public class IndexModel
+    //{
+
+    //}
+}
---
+++ DevCard/ViewComponents/ProjectViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard.ViewComponents
+{
+    public class ProjectViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("_Projects");
+        }
+    }
+}
